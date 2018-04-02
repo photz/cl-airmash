bf000b20a4c0630f674d1fde85a2aade51be9784
Philipp Hotz <p.hotz@posteo.net>
2018-04-02 19:58:04 +0200

    Remove obsolete read-stream-content-into-byte-vector

--- client.lisp
+++ client.lisp
@@ -4,30 +4,6 @@
 (in-package :airmash-client)
 
 
-(defun read-stream-content-into-byte-vector (stream &key ((%length length))
-                                                         (initial-size 4096))
-  "Return \"content\" of STREAM as freshly allocated (unsigned-byte 8) vector."
-  ;;(check-type length (or null non-negative-integer))
-  ;;(check-type initial-size positive-integer)
-  (do ((buffer (make-array (or length initial-size)
-                           :element-type '(unsigned-byte 8)))
-       (offset 0)
-       (offset-wanted 0))
-      ((or (/= offset-wanted offset)
-           (and length (>= offset length)))
-       (if (= offset (length buffer))
-           buffer
-           (subseq buffer 0 offset)))
-    (unless (zerop offset)
-      (let ((new-buffer (make-array (* 2 (length buffer))
-                                    :element-type '(unsigned-byte 8))))
-        (replace new-buffer buffer)
-        (setf buffer new-buffer)))
-    (setf offset-wanted (length buffer)
-          offset (read-sequence buffer stream :start offset))))
-
-
-
 (defun send-message (client player-command)
   (let ((res (flexi-streams:with-output-to-sequence (out-raw)
                (with-wrapped-in-bit-stream (out out-raw :byte-order :big-eendian)
